2615efb15baea1d7a10c9ff084b9c99beee60bc4
Joshua Jadulco <joshuajadulco@gmail.com>
2026-03-04 17:07:00 -0500

    feat(gta-alerts): setup prototype two theme foundation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,6 +40,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
 
